d547da81fac660a2357f113ccbef15748a88be14
Tyler649000 <134805689+Tyler649000@users.noreply.github.com>
2023-05-27 14:41:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function spanify(string) {
+    let spans = [];
+    for (let letter = 0; letter < string.length; letter++) {
+        let span = document.createElement('span');
+        span.innerHTML = string.charAt(letter);
+        spans.push(span);
+    }
+    return spans;
+}
+for (let title of document.querySelectorAll('.title')) {
+    let titleString = title.innerHTML;
+    title.innerHTML = '';
+    for (let span of spanify(titleString)) title.appendChild(span);
+}
+
+for (let video of document.getElementsByTagName('video')) {
+    video.parentElement.addEventListener('mouseenter', function() {
+        video.play();
+        video.playbackRate = 2;
+    });
+      
+    video.parentElement.addEventListener('mouseleave', function() {
+        video.pause();
+        video.currentTime = 0;
+    });
+
+    video.addEventListener('ended', function() {
+        video.play();
+    });
+}
